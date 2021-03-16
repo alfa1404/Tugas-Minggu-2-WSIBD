9557c104c298d9b019a1e138da75233ca0a99650
alfa1404 <80755786+alfa1404@users.noreply.github.com>
2021-03-16 18:55:51 +0700

    Create 1.File Person.java

---
+++ 1.File Person.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package latihan.pkg2;
+
+/**
+ *
+ * @author Bravo 15
+ */
+public class Latihan2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        
+        String fName = "Achmad Amreza";
+        String lName = "Alfarit";
+        String stuId = "E41202326";
+        String stuStatus = "Active";
+        
+        System.out.println("Student Name: " + fName + " " + lName);
+        System.out.println("Student ID: " + stuId);
+        System.out.println("Student Status: " + stuStatus);
+        
+    }
+    
+    
+}
